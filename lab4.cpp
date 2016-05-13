b1e4f4410e5655bba552da2c7424869f3ed68e06
WilmerFloresMolina <wilmerflores-tux2@unitec.edu>
2016-05-13 15:22:05 -0600

    avanzondo en ejercio 2

--- lab4.cpp
+++ lab4.cpp
@@ -5,24 +5,29 @@
 	#include <ncurses.h>
 	#include <fstream>
 
+
 	using std::cout;
 	using std::endl;
 	using std::cin;
 
-	void CreandoMatriz (int* numeros);
-	void liberar_Memoria(int* numeros);
+	void CreandoMatriz (double* numeros) ;
+	void liberar_Memoria(double* numeros,char* palabras);
+	double factorizar(double* numeros);
 
 
 	int main(int argc, char const *argv[]){
-		int* numeros = new int[5];
+		double* numeros = new double[6];
+		char* palabras = new char[100];
+	//	CreandoMatriz (numeros);
 		int opcion= 0;
 		while(opcion<3){
+
 			cout<<"1. Ejercicio 1"<<endl;
 			cout<<"2. Ejercicio 2"<<endl;
 			cout<<"3. Salir"<<endl;
 			cin >> opcion;
 			if(opcion==1){
-				int a=0,b=0,c=0;
+				double a=0,b=0,c=0;
 				cout<< "ingrese a"<<endl;
 				cin>>a;
 				cout << "ingrese b"<<endl;
@@ -33,6 +38,27 @@
 				numeros[1]=b;
 				numeros[2]=c;
 
+				if(factorizar(numeros) == 0){
+					cout<<"x1 = "<<numeros[4]<<endl;
+					cout<<"x2 = "<<numeros[5]<<endl;
+					if(numeros[4]>0){
+						cout<< "(x" <<(-1)*numeros[4]<<")"<<endl;
+					}else{
+						cout<< "(x" <<"+"<<(-1)*numeros[4]<<")"<<endl;
+					}
+					if(numeros[5]>0){
+						cout<< "(x" <<(-1)*numeros[5]<<")"<<endl;
+					}else{
+						cout<< "(x" <<"+"<<(-1)*numeros[5]<<")"<<endl;
+
+					}
+					
+					
+
+
+				}else{
+					cout<<"El programa no puede resolver numeros imaginarios :("<<endl;
+				}
 
 
 
@@ -41,46 +67,65 @@
 			}
 
 			if(opcion==2){
-				
+				cout<<"ingrese una palabra"<<endl;
+				cin>>palabras;
+				int contador=0;
+				for(int i =0;i<100;i++){
+					if(palabras[i] == '\0'){
+						contador=i;
+						break;
+					}
+
+				}
+				cout<<contador<<endl;
 			}
 
 
 		}
 
-
+		liberar_Memoria(numeros,palabras);
 		
 		return 0;
 	}
 
-	void liberar_Memoria(double* numeros){
+	void liberar_Memoria(double* numeros,char* palabras){
 
 		delete[] numeros;
+		delete[] palabras;
 	}
 
 	void CreandoMatriz (double* numeros){
-		for(int i =0; i<5;i++){
+		for(int i =0; i<6;i++){
 			numeros[i] = 0;
 		}
 	}
-	int factorizar(double* numeros){
+	double factorizar(double* numeros){
 		double a=0,b=0,c=0;
 		a=numeros[0];
 		b=numeros[1];
 		c=numeros[2];
+		double ret=0;
 
-		double argumento=sqrt(pow(b,2);
-		double abc=4*a*c;
-		double total=0;
-		total = argumento -abc;
+		double argumento=sqrt(pow(b,2));
+		double total1=0;
+		total1 = ((pow(b,2)-4*a*c));
+		
 
-		if(total>=0){
+		if(0<=total1){
 			double x1 = (-b + sqrt(pow(b,2)-4*a*c))/(2*a);
 			double x2 = (-b - sqrt(pow(b,2)-4*a*c))/(2*a);
-
+			numeros[4] =x1;
+			numeros[5] =x2; 
 		}else{
-			return 1;
+			ret =1;
 		}
 		
 
-		return 0;
+
+		
+			
+		
+		
+
+		return ret;
 	}
